476601c07a9c35e25d75d2362b08b07ec441b83c
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2025-11-03 02:04:38 -0700

    Update block_ads.sh

--- block_ads.sh
+++ block_ads.sh
@@ -23,7 +23,7 @@ function silent_error() {
 }
 
 # Download the latest domains list
-curl -sSfL --retry "$MAX_RETRIES" --retry-all-errors https://small.oisd.nl/domainswild2 | grep -vE '^\s*(#|$)' > oisd_small_domainswild2.txt || silent_error "Failed to download the domains list"
+curl -sSfL --retry "$MAX_RETRIES" --retry-all-errors https://github.com/badmojr/1Hosts/blob/master/Lite/domains.wildcards | grep -vE '^\s*(#|$)' > oisd_small_domainswild2.txt || silent_error "Failed to download the domains list"
 
 # Check if the file has changed
 git diff --exit-code oisd_small_domainswild2.txt > /dev/null && silent_error "The domains list has not changed"
