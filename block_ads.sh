941a8fc3a805bb614e56160f8d343bf209b4f4bb
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2025-11-20 07:52:25 -0700

    Update block_ads.sh

--- block_ads.sh
+++ block_ads.sh
@@ -21,8 +21,8 @@ TARGET_BRANCH="${GITHUB_REF_NAME:-$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 
 # --- Aggregator Configuration ---
 LIST_URLS=(
-    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate-onlydomains.txt" # Hagezi Ultimate
-    "https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/domains.wildcards" # 1Hosts Lite
+    #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate-onlydomains.txt" # Hagezi Ultimate
+    #"https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/domains.wildcards" # 1Hosts Lite
     "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/domainswild2_small.txt" #OISD Small
 )
 
